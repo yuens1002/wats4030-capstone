5dccc25256dc6550e0f39a3c2e25cfaac76130ac
Mike G <mkgalligar@gmail.com>
2017-02-08 14:25:01 -0800

    Got Lat & Lnog to print on site

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,7 @@
 angular.module('wats4030CapstoneApp')
   .controller('MainCtrl', function($scope, current) {
     $scope.current = current.query();
-    
+
     $scope.refreshCurrent = function() {
       $scope.current = current.query({
         location: $scope.location
--- app/scripts/services/current.js
+++ app/scripts/services/current.js
@@ -11,12 +11,14 @@ angular.module('wats4030CapstoneApp')
   .factory('current', function($resource) {
     // Service logic
     // ...
+//'http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f'
+//'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDHcLJvjxGIE2Z3Hk3jKN2WZhB73fXAt4c'
     // Public API here
-    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=600ee3b60b8fe48d87def46be2f0e45f', {}, {
+    return $resource('https://maps.googleapis.com/maps/api/geocode/json?address=:location&key=AIzaSyDHcLJvjxGIE2Z3Hk3jKN2WZhB73fXAt4c', {}, {
       query: {
         method: 'GET',
         params: {
-          location: 'Seattle,us'
+          location: '1600+Amphitheatre+Parkway,+Mountain+View,+CA'
         },
         isArray: false
       }
