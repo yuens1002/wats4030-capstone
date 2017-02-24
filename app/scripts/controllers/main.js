ee9f1ea7c362103205006081741bac376bfe4b6a
Mike G <mkgalligar@gmail.com>
2017-02-23 19:25:51 -0800

    2/23/17 tidyup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,21 +8,31 @@
  * Controller of the wats4030CapstoneApp
  */
 angular.module('wats4030CapstoneApp')
-  .controller('MainCtrl', function($scope, current, repsearch) {
+  .controller('MainCtrl', function($scope, current, repsearchfed, repsearch) {
     $scope.current = current.query();
 
-      $scope.refreshCurrent = function(location) {
-        $scope.current = current.query({
-          location: location
+    $scope.refreshCurrent = function(location) {
+      $scope.current = current.query({
+        location: location
+      });
+      $scope.current.$promise.then(function(data) {
+        $scope.repsearchfed = repsearchfed.query({
+          lat: data.results[0].geometry.location.lat, //This is the Google search
+          lng: data.results[0].geometry.location.lng
+        }).then(function(repdata) {
+          $scope.repdata = repdata.data;
         });
-        $scope.current.$promise.then(function(data) {
-          $scope.repsearch = repsearch.query({
-            lat: data.results[0].geometry.location.lat,//This is the Google search
-            lng: data.results[0].geometry.location.lng
-          });
-        });
-      };
+      });
 
 
+      $scope.current.$promise.then(function(data) {
+        $scope.repsearch = repsearch.query({
+          lat: data.results[0].geometry.location.lat, //This is the Google search
+          lng: data.results[0].geometry.location.lng
+        });
+      });
+
+
+    };
 
   });
--- app/scripts/services/repsearchfed.js
+++ app/scripts/services/repsearchfed.js
@@ -10,21 +10,30 @@
    lat: '47.625253',
    lng: '-122.29142'
  };
+ Reference: https://docs.angularjs.org/api/ng/service/$http
+ JSONP requests must specify a callback to be used in the response from the server.
+ This callback is passed as a query parameter in the request.
+ You must specify the name of this parameter by setting the jsonpCallbackParam
+ property on the request config object.
+$http.jsonp('some/trusted/url', {jsonpCallbackParam: 'callback'})
 
  */
-https://congress.api.sunlightfoundation.com/legislators/locate?latitude=47.625253&longitude=-122.29142
-angular.module('wats4030CapstoneApp')
-  .factory('repsearch', function($http) {
-    var sunAPI = 'https://congress.api.sunlightfoundation.com/legislators/locate?latitude=:lat&longitude=:lng';
-    return {
-      query: function(){
-      /*  params: {
-          method: 'GET',
-          lat: '47.625253'
-          lng: '-122.29142'
-        };*/
-        return $http.jsonp(sunAPI);
 
+//congress.api.sunlightfoundation.com/legislators/locate?latitude=47.625253&longitude=-122.29142
+angular.module('wats4030CapstoneApp')
+  .factory('repsearchfed', function($http) {
+    var sunAPI = 'https://congress.api.sunlightfoundation.com/legislators/locate';
+    return {
+      query: function(coords){//Turn prams into objects. This function will serialize
+        //Latitude & Longitude + the var lat lng and append them to the url as get parameters
+
+        return $http.jsonp(sunAPI, {  //
+          jsonpCallbackParam: 'callback',
+          params: {
+            latitude: coords.lat,//Turn lat into an object
+            longitude: coords.lng
+          }
+        });
       }
     };
   });
